2839c00d7d76213a6d1e4a6c4ac49e0ac3d5b167
MatheusMohr <matheusmohr000@gmail.com>
2025-04-03 21:14:05 -0300

    Validação nome email e celular

--- SistemaContatos/Models/Contato.cs
+++ SistemaContatos/Models/Contato.cs
@@ -1,10 +1,15 @@
-﻿namespace SistemaContatos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaContatos.Models
 {
     public class Contato
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="O campo nome é obrigatório")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo e-mail é obrigatório")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo celular é obrigatório")]
         public string Celular { get; set; }
     }
 }
