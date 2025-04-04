fa912f02da8826496bd5f5a4fbaa3be2f18d6cdc
MatheusMohr <matheusmohr000@gmail.com>
2025-04-03 21:19:31 -0300

    Update contato

--- SistemaContatos/Models/Contato.cs
+++ SistemaContatos/Models/Contato.cs
@@ -5,11 +5,13 @@ namespace SistemaContatos.Models
     public class Contato
     {
         public int Id { get; set; }
-        [Required(ErrorMessage="O campo nome é obrigatório")]
+        [Required(ErrorMessage="O campo nome é obrigatório!")]
         public string Nome { get; set; }
-        [Required(ErrorMessage = "O campo e-mail é obrigatório")]
+        [Required(ErrorMessage = "O campo e-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage ="E-mail inválido!")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "O campo celular é obrigatório")]
+        [Required(ErrorMessage = "O número de celular é obrigatório!")]
+        [Phone(ErrorMessage ="Número do celular inválido!")]
         public string Celular { get; set; }
     }
 }
